5d59d2d89ce04fd203eff9f78baa1d4e58f994e8
Abraham Yance <122916029+IsYancem@users.noreply.github.com>
2025-06-09 15:25:07 -0500

    seccion 10 video 129

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -16,6 +17,8 @@ import { ConfigModule } from '@nestjs/config';
       autoLoadEntities: true,
       synchronize: true,
     }),
+
+    ProductsModule,
   ],
 })
 export class AppModule {}
